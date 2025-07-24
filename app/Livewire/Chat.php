632c5397b4a7b207e8f134af3e51be07d898f70d
fmuiin <fathulmuiin14@gmail.com>
2025-07-24 12:16:14 +0700

    debug1 chat

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -10,13 +11,40 @@ class Chat extends Component
 {
     public $users;
     public $selectedUser;
-    
+    public $newMessage;
+    public $messages;
+
     public function mount()
     {
         $this->users = User::whereNot("id", Auth::id())->get();
         $this->selectedUser = $this->users->first();
+        $this->messages = ChatMessage::query()
+            ->where(function($query) {
+                $query->where("sender_id", Auth::id())
+                    ->where("receiver_id", $this->selectedUser->id);
+            })
+            ->orWhere(function($query) {
+                $query->where("sender_id", $this->selectedUser->id)
+                    ->where("receiver_id", Auth::id());
+            })->latest()->get();
+
     }
-    
+    public function selectUser($userId)
+    {
+        $this->selectedUser = User::find($userId);
+    }
+
+    public function submit()
+    {
+        if(!$this->newMessage) return;
+
+        ChatMessage::create([
+            "sender_id" => Auth::id(),
+            "receiver_id" => $this->selectedUser->id,
+            "message" => $this->newMessage
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.chat');
--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -13,7 +13,8 @@
             <div class="divide-y">
 
             @foreach ($users as $user)
-                <div class="p-3 cursor-pointer hover:bg-blue-100 transition">
+                <div wire:click="selectUser({{ $user->id }})" class="p-3 cursor-pointer hover:bg-blue-100 transition 
+                    {{ $selectedUser->id === $user->id ? 'bg-blue-50 font-semibold' : '' }}">
                     <div class="text-gray-800">{{$user->name}}</div>
                     <div class="text-xs text-gray-500">{{$user->email}}</div>
                 </div>
@@ -31,17 +32,19 @@
     
             <!-- Messages -->
             <div class="flex-1 p-4 overflow-y-auto space-y-2 bg-gray-50">
-                <div class="flex justify-end">
-                    <div class="max-w-xs px-4 py-2 rounded-2xl shadow bg-blue-600 text-white">
-                        Hi, This is test
+                @foreach ($messages as $message)
+                    <div class="flex justify-end">
+                        <div class="max-w-xs px-4 py-2 rounded-2xl shadow bg-blue-600 text-white">
+                            {{ $message->message }}
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
     
             <!-- Input -->
-            <form class="p-4 border-t bg-white flex items-center gap-2">
+            <form wire:submit="submit" class="p-4 border-t bg-white flex items-center gap-2">
                 <input 
-                    type="text"
+                    type="text" wire:model="newMessage"
                     class="flex-1 border border-gray-300 rounded-full px-4 py-2 text-sm focus:outline-none focus:ring focus:ring-blue-300"
                     placeholder="Type your message..." />
                 <button type="submit"
